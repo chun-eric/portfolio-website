a769a060c58bdbb68ab9c7b1aacf6b043fb25c27
chun-eric <eric.chun88800888@gmail.com>
2023-03-15 14:19:58 +0900

    mostly completed form except refresh

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
 const bar = document.getElementById('bar');
 const navbar = document.getElementById('navbar');
 const close = document.getElementById('close');
-console.log(close);
+
 
 bar.addEventListener("click", () => {
     navbar.classList.add('activate');
@@ -29,17 +29,15 @@ close.addEventListener('click', () => {
 
 
 
-/* Portfolio Works */
+/* Portfolio Filtering */
 
 //selecting elements
 const gallery = document.querySelector(".gallery");
 const itemboxes = document.querySelectorAll(".itembox");
-console.log(itemboxes);
+
 
 // Click events
 gallery.addEventListener("click", (e) => {
-   
-
     if (e.target.classList.contains("filter")) {
         //#1 removing the active class in original state
         gallery.querySelector(".active").classList.remove("active");
@@ -51,6 +49,7 @@ gallery.addEventListener("click", (e) => {
         const filterValue = e.target.getAttribute("data-filter");
 
         //#4 Looping through all the itemboxes & checking if each item contains filterValue
+
         //#5 Adding a show class and removing a hide class
         itemboxes.forEach(item => {
             if(item.classList.contains(filterValue) || filterValue === "all") {
@@ -66,3 +65,68 @@ gallery.addEventListener("click", (e) => {
     }
 
 });
+
+
+
+
+
+/* Form Submission and Error Handling */
+const emailInput = document.getElementById("email")
+const emailError = document.getElementById("email-error")
+const emailSuccess = document.getElementById("email-success")
+const form = document.getElementById("form");
+const submitButton = document.getElementById("submit--button")
+
+console.log(form);
+
+// remove error styling
+function removeError() {
+    emailError.innerHTML="";
+    emailInput.style.borderBottomColor = "transparent"
+}
+
+// remove success styling
+function removeSuccess() {
+    emailSuccess.innerHTML="";
+    emailInput.style.borderBottomColor = "transparent"
+
+}
+
+// clearing all inputs
+function clearAll () {
+    form.reset();
+}
+
+// validating email
+function validateEmail() {
+  
+    if (!emailInput.value.match(/^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)) {
+        emailError.innerHTML = "Please enter a valid email."
+        emailError.style.top = "120%"
+        emailInput.style.borderBottomColor = "red"
+        removeSuccess()
+
+        if (emailInput.value == "") {
+            removeError();
+            return true;
+        }
+        return false;
+
+    } 
+        emailSuccess.innerHTML ="Success!";
+        emailSuccess.style.top = "120%"
+        emailInput.style.borderBottomColor = "rgb(64, 179, 11)";
+        removeError();
+        return true;   
+}
+
+
+// changing button effect when clicked 
+submitButton.addEventListener("click", () => {
+    // adding a button effect
+    submitButton.classList.add("submit--btn");
+})
+
+form.addEventListener("submit", () => {
+    form.reset();
+})
